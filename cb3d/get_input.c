4143098cbbc98b8755bfff4f4ac125b6b6ffe5cf
rafa <askunknownsurname@gmail.com>
2022-12-03 03:27:28 -0300

    norminette and some QL changes on get_input

--- cb3d/get_input.c
+++ cb3d/get_input.c
@@ -1,43 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_input.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpaulino <rpaulino@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/03 03:19:02 by rpaulino          #+#    #+#             */
+/*   Updated: 2022/12/03 03:25:45 by rpaulino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cube3d.h"
 
-int get_fd(char *argv)
+int	get_fd(char *argv)
 {
-    int fd;
+	int	fd;
 
-    fd = open(argv, O_RDONLY);
-	if(fd == -1)
-		throw_error("Can't get File descriptor");
-	else
-		printf("Success! fd:%d file:%s\n", fd, argv);
-    return (fd);
+	fd = open(argv, O_RDONLY);
+	if (fd == -1)
+		throw_error("ERROR: Can't get File descriptor");
+	return (fd);
 }
 
 char	*read_data(int fd)
 {
-	char    *buffer;
-	char    *temp;
+	char	*buffer;
+	char	*temp;
 	char	*temporary;
-    size_t	n;
+	size_t	n;
 
 	temp = (char *)malloc(sizeof(char) + 1);
-    buffer = strdup("");
-    n = read(fd, temp, 1);
+	buffer = strdup("");
+	n = read(fd, temp, 1);
 	temp[1] = '\0';
 	while (n > 0)
-    {
+	{
 		temporary = ft_strjoin(buffer, temp);
-        free(buffer);
+		free(buffer);
 		buffer = temporary;
-        n = read(fd, temp, 1);
-    }
+		n = read(fd, temp, 1);
+	}
 	close(fd);
 	free(temp);
-    return (buffer);
+	return (buffer);
 }
 
 char	*get_data_from_file(int argc, char *argv[])
 {
-	int     fd;
+	int	fd;
 
 	(void)argc;
 	fd = get_fd(argv[1]);
