9415aa72fd9ef3b0d990cc3ea2f7d16489ca4c57
Antonio Silva <anjose-d@student.42sp.org.br>
2022-12-03 10:37:59 -0300

    including libft into the project

--- prod/includes/cub3d.h
+++ prod/includes/cub3d.h
@@ -1,6 +1,6 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include <stdio.h>
+# include <libft.h>
 
 #endif
--- prod/srcs/main.c
+++ prod/srcs/main.c
@@ -2,7 +2,7 @@
 
 int	main(int argc, char *argv[])
 {
-	printf("Hello, world!\n");
-	
+	ft_printf("Hello, world!\n");
+
 	return (0);
 }
