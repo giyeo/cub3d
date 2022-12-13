282941aa63660c0319976f8922d39beb8a3baf1b
Antonio Silva <anjose-d@student.42sp.org.br>
2022-12-13 18:52:07 -0300

    setting up the first parser srcs to be tested

--- anjose-d/test_srcs/tests/test_ft_strlen.c
+++
@@ -1,6 +0,0 @@
-#include "test_main.h"
-
-void	test1()
-{
-	TEST_ASSERT_EQUAL(4, ft_strlen("asdf"));
-}
--- anjose-d/test_srcs/tests/test_outra.c
+++
@@ -1,9 +0,0 @@
-#include "test_main.h"
-
-#define EXPECTED "asdf"
-
-void	coisa()
-{
-	g_str = ft_strdup(EXPECTED);
-	TEST_ASSERT_EQUAL_STRING(EXPECTED, g_str);
-}
