1670c6a2f8437d8bca661ba4da69d6cc314b2693
rafa <askunknownsurname@gmail.com>
2022-12-20 12:10:43 -0300

    Some norminette fixes

--- prod/srcs/main.c
+++ prod/srcs/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpaulino <rpaulino@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/20 12:02:23 by rpaulino          #+#    #+#             */
+/*   Updated: 2022/12/20 12:02:45 by rpaulino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <cub3d.h>
 
 int	main(int argc, char *argv[])
@@ -12,7 +24,6 @@ int	main(int argc, char *argv[])
 	close(fd);
 	parser_and_validate(buffer, &config);
 	ft_destroy_matrix(buffer);
-	// run (&config);
 	print_struct(&config);
 	free_config(&config);
 	return (0);
--- prod/srcs/parser/validate_config.c
+++ prod/srcs/parser/validate_config.c
@@ -6,7 +6,7 @@
 /*   By: rpaulino <rpaulino@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 21:49:51 by rpaulino          #+#    #+#             */
-/*   Updated: 2022/12/20 11:53:09 by rpaulino         ###   ########.fr       */
+/*   Updated: 2022/12/20 12:09:14 by rpaulino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	assign_color(char type, t_config *config, int *color)
 {
-	if(type == 'F' && config->F[0] == -1)
+	if (type == 'F' && config->F[0] == -1)
 	{
 		config->F[0] = color[0];
 		config->F[1] = color[1];
@@ -32,24 +32,25 @@ void	assign_color(char type, t_config *config, int *color)
 
 void	parse_color(char *file_content, char type, t_config *config)
 {
-	int i = 0;
-	int comma_index = 0;
-	int start_index = 0;
-	char *number_string;
-	char *trimmed_number_string;
-	int *color;
+	int		i;
+	int		comma_index;
+	int		start_index;
+	char	*number_string;
+	char	*trimmed_number_string;
+	int		*color;
 
+	i = 0;
+	start_index = 0;
+	comma_index = 0;
 	color = (int *)(malloc(sizeof(int) * 3));
-	while(i < 3)
-	{	//FIND WHERE END
+	while (i < 3)
+	{
 		comma_index = ft_str_find_idx(file_content + start_index, ',');
-		if(comma_index == -1) //CASE LAST NUMBER
+		if (comma_index == -1)
 			comma_index = ft_str_find_idx(file_content + start_index, '\0');
-		//GET STRING AND PARSE TO NUMBER
 		number_string = ft_substr(file_content, start_index, comma_index);
 		trimmed_number_string = ft_strtrim(number_string, " ");
 		color[i++] = ft_atoi(trimmed_number_string);
-		//REPEAT
 		start_index += comma_index + 1;
 		free(number_string);
 		free(trimmed_number_string);
@@ -61,7 +62,7 @@ void	parse_color(char *file_content, char type, t_config *config)
 void	test_path(char *path)
 {
 	int		fd;
-	char 	*temp;
+	char	*temp;
 
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
@@ -78,9 +79,9 @@ void	assign_config(char *path, char type, t_config *config)
 		config->NO = path;
 	else if (type == 'W' && config->WE == NULL)
 		config->WE = path;
-	else if(type == 'S' && config->SO == NULL)
+	else if (type == 'S' && config->SO == NULL)
 		config->SO = path;
-	else if(type == 'E' && config->EA == NULL)
+	else if (type == 'E' && config->EA == NULL)
 		config->EA = path;
 	else
 		throw_error("More Than one Texture");
@@ -94,21 +95,20 @@ void	parse_path(char *file_content, char type, t_config *config)
 	path = ft_substr(file_content, 0, ft_str_find_idx(file_content, '\n'));
 	trimmed_path = ft_strtrim(path, " ");
 	test_path(trimmed_path);
-	
 	assign_config(trimmed_path, type, config);
 	free(path);
 }
 
 int	check_invalid_char_or_map_start(char c)
 {
-	if(c == '1')
+	if (c == '1')
 		return (1);
 	if (c != 'N' && c != 'O' && c != 'S'
 		&& c != 'W' && c != 'E' && c != 'A'
 		&& c != 'F' && c != 'C' && c != ' '
 		&& c != '\n' && c != '\r')
-			throw_error("Not Allowed Char in .cub file");
-	return(0);
+		throw_error("Not Allowed Char in .cub file");
+	return (0);
 }
 
 int	can_parse(char *line_content, int i)
@@ -136,29 +136,33 @@ int	can_parse(char *line_content, int i)
 
 void	color_error_handling(char *line_content)
 {
-	int i = 0;
-	int count_commas = 0;
-	int has_digit = 0;
-	while(line_content[i] != '\0')
+	int	count_commas;
+	int	has_digit;
+	int	i;
+
+	count_commas = 0;
+	has_digit = 0;
+	i = 0;
+	while (line_content[i] != '\0')
 	{
-		if(!ft_isdigit(line_content[i])
-		&& line_content[i] != ' '
-		&& line_content[i] != ',')
+		if (!ft_isdigit(line_content[i])
+			&& line_content[i] != ' '
+			&& line_content[i] != ',')
 			throw_error("Invalid Char on Color");
-		if(ft_isdigit(line_content[i]))
+		if (ft_isdigit(line_content[i]))
 			has_digit = 1;
-		if(line_content[i] == ',')
+		if (line_content[i] == ',')
 		{
-			if(has_digit == 0)
+			if (has_digit == 0)
 				throw_error("Not enough digits");
 			count_commas++;
 			has_digit = 0;
 		}
 		i++;
 	}
-	if(has_digit == 0)
+	if (has_digit == 0)
 		throw_error("Not enough digits");
-	if(count_commas != 2)
+	if (count_commas != 2)
 		throw_error("Only 2 commas allowed");
 }
 
@@ -187,13 +191,13 @@ void	validate_config(char **buffer, t_config *config)
 		column = 0;
 		while (line_content[column] != '\0')
 		{
-			if(check_invalid_char_or_map_start(line_content[column]))
+			if (check_invalid_char_or_map_start(line_content[column]))
 				return ;
 			type = can_parse(line_content, column);
 			if (type != 0)
 			{
 				parse_line_content(line_content + column, (char)type, config);
-				break;
+				break ;
 			}
 			column++;
 		}
