5927ed2ddf43e4fe13ac1d1d0692b587c5e70fce
Antonio Silva <anjose-d@student.42sp.org.br>
2022-12-28 16:54:20 -0300

    testing open window

--- anjose-d-play/srcs/main.c
+++ anjose-d-play/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: anjose-d <anjose-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 12:02:23 by rpaulino          #+#    #+#             */
-/*   Updated: 2022/12/28 16:37:35 by anjose-d         ###   ########.fr       */
+/*   Updated: 2022/12/28 16:49:18 by anjose-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	main(int argc, char *argv[])
 	t_config	config;
 	char		**buffer;
 	int			fd;
+	t_conn		conn_mlx;
 
 	config_init(&config);
 	fd = file_validate(argv[1], argc);
@@ -24,7 +25,9 @@ int	main(int argc, char *argv[])
 	close(fd);
 	parser_and_validate(buffer, &config);
 
-	
+	conn_mlx.mlx_ptr = mlx_init();
+	conn_mlx.win_ptr = mlx_new_window(conn_mlx.mlx_ptr, 600, 600, "TEST");
+	mlx_loop(conn_mlx.mlx_ptr);
 
 	ft_destroy_matrix(buffer);
 	free_config(&config);
