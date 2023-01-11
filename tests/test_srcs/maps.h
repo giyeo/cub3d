c2cbc5e4859622c06c6e1d16dd50ea66ceaf68c8
Antonio Silva <anjose-d@student.42sp.org.br>
2023-01-11 10:36:36 -0300

    changing maps path

--- tests/test_srcs/maps.h
+++ tests/test_srcs/maps.h
@@ -2,22 +2,22 @@
 # define MAPS_H
 /* maps */
 // SIMPLE
-# define SUBJECT_MAP "../maps_for_test/simple/subject.cub"
-# define DOUBLE_ROOM_MAP "../maps_for_test/simple/DOUBLE_ROOM_SIMPLE_MAP.cub"
-# define DOUBLE_HOLE_MAP "../maps_for_test/simple/BIG_ROOM_HOLES_SIMPLE_MAP.cub"
-# define MANY_BLINES_MAP "../maps_for_test/simple/several_breaklines.cub"
+# define SUBJECT_MAP "maps_for_test/simple/subject.cub"
+# define DOUBLE_ROOM_MAP "maps_for_test/simple/DOUBLE_ROOM_SIMPLE_MAP.cub"
+# define DOUBLE_HOLE_MAP "maps_for_test/simple/BIG_ROOM_HOLES_SIMPLE_MAP.cub"
+# define MANY_BLINES_MAP "maps_for_test/simple/several_breaklines.cub"
 
 // WRONG
-# define NO_PERM_MAP "../maps_for_test/wrong/noperm.cub"
-# define INVALID_MAP "../maps_for_test/wrong/invalid_char_in_map.cub"
-# define ENTRANCE_TOP_MAP "../maps_for_test/wrong/entrance_on_top.cub"
-# define ENTRANCE_LEFT_MAP "../maps_for_test/wrong/entrance_on_the_left.cub"
-# define ENTRANCE_RIGHT_MAP "../maps_for_test/wrong/entrance_on_the_right.cub"
-# define ENTRANCE_BOTTOM_MAP "../maps_for_test/wrong/entrance_on_the_bottom.cub"
-# define OPEN_ON_TOP_MAP "../maps_for_test/wrong/map_open_on_top.cub"
-# define OPEN_ON_BOTTOM_MAP "../maps_for_test/wrong/map_open_on_bottom.cub"
-# define OPEN_INVREAD_MAP "../maps_for_test/wrong/map_open_invalidread.cub"
-# define TWO_PLAYERS_MAP "../maps_for_test/wrong/SUBJECT_TWO_PLAYERS.cub"
-# define GLUED_MAP "../maps_for_test/simple/glued-map.cub"
+# define NO_PERM_MAP "maps_for_test/wrong/noperm.cub"
+# define INVALID_MAP "maps_for_test/wrong/invalid_char_in_map.cub"
+# define ENTRANCE_TOP_MAP "maps_for_test/wrong/entrance_on_top.cub"
+# define ENTRANCE_LEFT_MAP "maps_for_test/wrong/entrance_on_the_left.cub"
+# define ENTRANCE_RIGHT_MAP "maps_for_test/wrong/entrance_on_the_right.cub"
+# define ENTRANCE_BOTTOM_MAP "maps_for_test/wrong/entrance_on_the_bottom.cub"
+# define OPEN_ON_TOP_MAP "maps_for_test/wrong/map_open_on_top.cub"
+# define OPEN_ON_BOTTOM_MAP "maps_for_test/wrong/map_open_on_bottom.cub"
+# define OPEN_INVREAD_MAP "maps_for_test/wrong/map_open_invalidread.cub"
+# define TWO_PLAYERS_MAP "maps_for_test/wrong/SUBJECT_TWO_PLAYERS.cub"
+# define GLUED_MAP "maps_for_test/simple/glued-map.cub"
 
 #endif
