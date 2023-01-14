75bf8843027fd7a3cc9bcf23d350486b9e588058
rafa <42rpaulino@gmail.com>
2023-01-13 22:39:59 -0300

    remove space to tab

--- MANDATORY/srcs/render/img_pix_put.c
+++ MANDATORY/srcs/render/img_pix_put.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   img_pix_put.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anjose-d <anjose-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: rpaulino <rpaulino@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 17:52:47 by anjose-d          #+#    #+#             */
-/*   Updated: 2023/01/02 20:55:06 by anjose-d         ###   ########.fr       */
+/*   Updated: 2023/01/13 22:39:17 by rpaulino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	img_pix_put(t_img *img, int x, int y, int color)
 {
-	char *pixel;
+	char	*pixel;
 
 	if (y < WINDOW_HEIGHT && x < WINDOW_WIDTH)
 	{
